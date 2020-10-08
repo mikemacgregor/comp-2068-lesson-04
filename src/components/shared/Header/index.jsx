f7ab34b5f6f5736e10c21d053b0e6a1b701bf966
mikemacgregor <mike.macgregor@rogers.com>
2020-10-08 18:01:04 -0400

    initial commit

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Styles from './styles.jsx';
 
-const Header = () => {
-  return ();
+const Header = (props) => {
+  return (
+    <Styles.Header>
+      <h1>{props.title}</h1>
+    </Styles.Header>
+  );
 }
  
 export default Header;
